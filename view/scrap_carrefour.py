9243b98f8bec703f245f6bfa53f85b0118791f6e
damianra <44167699+damianra@users.noreply.github.com>
2021-09-16 12:35:34 -0300

    Update scrap_carrefour.py

--- view/scrap_carrefour.py
+++ view/scrap_carrefour.py
@@ -2,6 +2,7 @@ import time
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from webdriver_manager.chrome import ChromeDriverManager
+import os
 
 def seleniumScrap(url, browser, dic):
     # Open url in navigator
@@ -18,10 +19,12 @@ def seleniumScrap(url, browser, dic):
 
 
 def carr_scrap(links):
-    option = webdriver.ChromeOptions()
-    # second plane browser option
-    option.add_argument("headless")
-    browser = webdriver.Chrome(executable_path=ChromeDriverManager().install(),options=option)
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--no-sandbox")
+    browser = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
 
     product_price = {}
     # links = ["https://www.carrefour.com.ar/oblea-kit-kat-415-g/p", "https://www.carrefour.com.ar/limpiador-en-crema-cif-original-multiuso-500-ml/p", "https://www.carrefour.com.ar/crema-uat-liviana-vitaminas-la-serenisima-tetra-top-200-cc/p", "https://www.carrefour.com.ar/papas-fritas-mc-cain-tradicional-400-g/p"]
